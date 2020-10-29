abaa00e0bec3d836e6c761795570b7604f8ae8ab
KarlaRamirezG <71740548+KarlaRamirezG@users.noreply.github.com>
2020-10-29 00:24:27 -0600

    2 formularios
    codigo

---
+++ dos formularios/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace dos_formularios
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connect = "datasource=localhost;port=3306;username=root;password=;database=facebook";
+            string query = "select * from usuarios where usuario = '" + textBox1.Text + "' AND contraseña = '" + textBox2.Text + "'";
+            MySqlConnection databaseConnection = new MySqlConnection(connect);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader reader = commandDatabase.ExecuteReader();
+                MessageBox.Show("registro completado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
